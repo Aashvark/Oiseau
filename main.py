09bd65786875865ba78d2b4d775681a6eab715bb
Ash Melvin <102767150+AshPixel@users.noreply.github.com>
2022-10-23 15:08:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from os import listdir, getcwd
+from sys import argv
+from packages.Lexer import Lexer
+from packages.Parser import Parser
+from packages.Evaluator import Evaluator
+from packages.ErrorManager import SimpleError
+
+class LanguageManager:
+    def __init__(self, filename):
+        file   = open(filename, 'r').read()
+        tokens = Lexer(file).tokens
+        ast    = Parser(tokens).ast
+
+        try: Evaluator(ast)
+        except: # SimpleError("UnexpectedError", "error was unexpected") 
+            pass
+
+if __name__ == '__main__':
+    if len(argv) == 1: SimpleError("FileError", "file wasn't given.")
+    
+    arg = argv[1].replace('\\', '/')
+    if arg.rsplit('.', 1)[1] not in ['oi', 'ois', 'ose']: SimpleError("FileError", "incorrect file type used.")
+    
+    path = f"{getcwd()}/{arg.removeprefix('/')}".replace('/', '\\') if not arg.lower().startswith('c:/') else arg.replace('/', '\\')
+    try: dirList = listdir(path.rsplit('\\', 1)[0])
+    except FileNotFoundError:
+        p = path.rsplit('\\', 1)[0]
+        SimpleError("FileError", f"no such file or directory")
+        
+    if not path.rsplit('\\', 1)[1] in dirList: SimpleError("FileError", "file given doesn't exist.")
+    
+    lm = LanguageManager(argv[1])
